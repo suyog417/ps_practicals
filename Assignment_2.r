62f6c537b5a31e26777a822f9cbe1859a373ea3a
suyog417 <78255834+suyog417@users.noreply.github.com>
2023-05-14 23:43:30 +0530

    Update Assignment_2.r

--- Assignment_2.r
+++ Assignment_2.r
@@ -2,12 +2,7 @@ condictional_probality <- function (a, b) {
   temp <- ( a / b)
   print(temp)
 }
-print ("Enter the No. of Accident who were following the rule: ")
-ap <- readline()
-ap <- as.integer(ap)
 ap=50 
-acc <- readline("Enter the No. of Accidents: ")
-acc <- as.integer(acc)
 acc = 2050
 print("Probability of Accidents happen even the person had followed the rules: ")
 condictional_probality(ap, acc)
